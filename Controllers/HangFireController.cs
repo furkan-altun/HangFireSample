b00374ff954dad8e8ecb1f06d573433ed731774b
furkan-altun <34065538+furkan-altun@users.noreply.github.com>
2021-12-21 13:08:23 +0300

    Add project files.

---
+++ Controllers/HangFireController.cs
@@ -0,0 +1,71 @@
+﻿using Hangfire;
+using HangFireSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HangFireSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangFireController : ControllerBase
+    {
+
+        MailHelper _mailHelper = new MailHelper();
+
+        [HttpPost]
+        [Route("welcome")]
+        public IActionResult Welcome(string mailAddress)
+        {
+            //Fire-and-Forget Jobs
+            //Fire - and - forget jobs are executed only once and almost immediately after creation.
+            var mailSubject = $"Welcome {mailAddress}";
+            var mailBody = $"<p>Thanks for join us!</p>";
+            var jobId = BackgroundJob.Enqueue(()=> _mailHelper.SendMail(mailAddress, mailSubject, mailBody));
+
+            return Ok($"JobId : {jobId} Completed. Welcome Mail Sent");
+        }
+
+        [HttpPost]
+        [Route("welcome-delayed")]
+        public IActionResult WelcomeDelayed(string mailAddress)
+        {
+            //Delayed Jobs
+            //Delayed jobs are executed only once too, but not immediately, after a certain time interval.
+            var mailSubject = $"Welcome {mailAddress}";
+            var mailBody = $"<p>Thanks for join us!</p></br><p>This mail is delayed mail.!</p>";
+            var jobId = BackgroundJob.Schedule(() => _mailHelper.SendMail(mailAddress, mailSubject, mailBody), TimeSpan.FromSeconds(20));
+
+            return Ok($"JobId : {jobId} Completed. Welcome Mail Sent");
+        }
+
+        [HttpPost]
+        [Route("welcome-recurring")]
+        public IActionResult WelcomeRecurring(string mailAddress)
+        {
+            //Recurring Jobs
+            //Recurring jobs fire many times on the specified CRON schedule.
+            var mailSubject = $"Welcome {mailAddress}";
+            var mailBody = $"<p>Thanks for join us!</p></br><p>This mail is recurring every minute</p>";
+            RecurringJob.AddOrUpdate(() => _mailHelper.SendMail(mailAddress, mailSubject, mailBody), Cron.Minutely);
+
+            return Ok($"Recurring job started, mails will send in every minute");
+        }
+
+        [HttpPost]
+        [Route("welcome-continuation")]
+        public IActionResult WelcomeContinuation(string mailAddress)
+        {
+            //Continuations Jobs
+            //Continuations are executed when its parent job has been finished.
+
+            //First job
+            var jobId = BackgroundJob.Enqueue(() => _mailHelper.NotifySystemAdmin());
+
+            var mailSubject = $"Welcome {mailAddress}";
+            var mailBody = $"<p>Thanks for join us!</p></br><p>This mail is continuations hangfire job example.<br>Job started after NotifySystemAdmin process</p>";
+            BackgroundJob.ContinueJobWith(jobId, () => _mailHelper.SendMail(mailAddress, mailSubject, mailBody));
+
+            return Ok($"Recurring job started, mails will send in every minute");
+        }
+    }
+}
---
+++ Models/MailHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace HangFireSample.Models
+{
+    public class MailHelper
+    {
+        public void SendMail(string username, string subject, string mailBody)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("hangfiresample@gmail.com");
+                mail.To.Add(username);
+
+                mail.Subject = subject;
+                mail.IsBodyHtml = true;
+                mail.Body = mailBody;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new NetworkCredential("hangfiresample@gmail.com", "hangfire123456");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void NotifySystemAdmin()
+        {
+            Console.WriteLine("Continuations will start soon...");
+        }
+    }
+}
